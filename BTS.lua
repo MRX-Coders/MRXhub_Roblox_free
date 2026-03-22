f2e43fa2fe8f9232f4425fa445d4375f4da14ae6
MRX-Coders <pyarnizerder@gmail.com>
2026-03-22 17:36:15 +0300

    Create BTS.lua

---
+++ BTS.lua
@@ -0,0 +1,56 @@
+return {
+    {
+        ["Name"] = "Emotes",
+        ["Callback"] = function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/7yd7/Hub/refs/heads/Branch/GUIS/Emotes.lua"))()
+        end
+    },
+    {
+        ["Name"] = "Infinite Yield",
+        ["Callback"] = function()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+        end
+    },
+    {
+        ["Name"] = "Btools",
+        ["Callback"] = function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/FBKJTUyw"))()
+        end
+    },
+    {
+        ["Name"] = "Fly Mode",
+        ["Callback"] = function()
+            print("Fly mode activated!") -- Сюда вставляй любой код
+        end
+    }
+    {
+        ["Name"] = "Tp object: hot key Home",
+        ["Callback"] = function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/2bAh83C2))()
+        end
+    },
+    {
+        ["Name"] = "Dash hot key E",
+        ["Callback"] = function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/yJuWhQd5"))()
+        end
+    },
+    {
+        ["Name"] = "Universal Hitbox MRX",
+        ["Callback"] = function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/V2PZnCjs"))()
+        end
+    },
+    {
+        ["Name"] = "Universal aimbot key U",
+        ["Callback"] = function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/aLZdLgSs"))()
+        end
+    },
+    {
+        ["Name"] = "ALL CONVEYOR EDITION key K",
+        ["Callback"] = function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/k4W6qbDF"))()
+        end
+    }
+}
